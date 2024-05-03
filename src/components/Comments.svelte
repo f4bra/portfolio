f48c6f3bf6915e87e95e11be967fe8a0194a08eb
f4bra <fabra8@gmail.com>
2024-05-03 13:03:29 +0200

    utterance comments

---
+++ src/components/Comments.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount } from "svelte";
+  onMount(() => {
+    const s = document.createElement("script");
+    const tag = document.getElementById("utterances");
+    s.src = "https://utteranc.es/client.js";
+    s.setAttribute("repo", "f4bra/portfolio");
+    s.setAttribute("issue-term", "pathname");
+    s.setAttribute("theme", "preferred-color-scheme");
+    s.setAttribute("crossorigin", "anonymous");
+    tag.parentNode.insertBefore(s, tag);
+  });
+</script>
+
+<div id="utterances"></div>
