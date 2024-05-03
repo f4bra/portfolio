77db2f4e4585c28096f4ea0b2e312f61c903ffce
f4bra <fabra8@gmail.com>
2024-05-03 13:03:04 +0200

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,17 @@ import aws from "astro-sst";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: aws(),
   site: 'https://f4bra.com',
-  integrations: [
-  mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'one-dark-pro' },
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'one-dark-pro'
     }
-  ), 
-  sitemap(), 
-  tailwind()
-  ],
+  }), sitemap(), tailwind(), svelte()]
 });
